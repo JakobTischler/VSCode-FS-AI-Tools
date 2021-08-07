d16142f4879eccca0faae12657f1db1c4b23407b
Jakob Tischler <jakob.tischler@gmail.com>
2021-08-07 16:28:31 +0200

    Clean aircraft code improvements and fixes

--- src/tools/aircraft-cfg/clean-v2.ts
+++ src/tools/aircraft-cfg/clean-v2.ts
@@ -10,162 +10,156 @@ export async function CleanAircraftCfg() {
 	const config = vscode.workspace.getConfiguration('fs-ai-tools', undefined);
 
 	const editor = vscode.window.activeTextEditor;
-	if (editor) {
-		const document = editor.document;
-		if ('file' === document.uri.scheme && document.uri.path.toLocaleLowerCase().endsWith('aircraft.cfg')) {
-			let text = document.getText();
-			// text = text.replace('\r', '');
-			const splitText = text.split('\n');
-			// const splitText = splitLines(text);
-			const textLineLength = splitText.length;
+	if (!editor) return;
 
-			/**
-			 * The final lines array.
-			 */
-			let cleanTextArray: string[] = [];
+	const document = editor.document;
 
-			/**
-			 * The current cfg sub-section in the line iteration.
-			 */
-			let currentSection: string | null = null;
-			let isFltsimSection: boolean = false;
+	if ('file' !== document.uri.scheme || !document.uri.path.toLocaleLowerCase().endsWith('aircraft.cfg')) return;
 
-			/**
-			 * The current fltsim entry's data.
-			 */
-			let currentFltsimEntry = createEmptyEntry();
+	// Start
+	const text = document.getText();
+	const splitText = text.split('\n');
 
-			/**
-			 * List of all entries, with each entry being an object
-			 */
-			const fltsimEntries: FltsimEntry[] = [];
+	/**
+	 * The final lines array.
+	 */
+	const cleanTextArray: string[] = [];
 
-			const removeProperties = new Map();
-			for (const prop of config.cleanAircraftCfg.removeUnusedLinesItems) {
-				let [key, value] = prop.split('=');
-				removeProperties.set(key, value);
-			}
+	/**
+	 * The current cfg sub-section in the line iteration.
+	 */
+	let currentSection: string | null = null;
+	let isFltsimSection = false;
 
-			// Split lines and iterate through
-			splitText.forEach((line, i) => {
-				line = line.trim();
+	/**
+	 * The current fltsim entry's data.
+	 */
+	let currentFltsimEntry = createEmptyEntry();
 
-				if (line.length > 0) {
-					let isSectionStart = line.startsWith('[') && line.endsWith(']');
-					// New section start
-					if (isSectionStart) {
-						// Close old section and push to list
-						if (currentSection && isFltsimSection) {
-							fltsimEntries.push(currentFltsimEntry);
-						}
+	/**
+	 * List of all entries, with each entry being an object
+	 */
+	const fltsimEntries: FltsimEntry[] = [];
 
-						currentSection = line;
-						isFltsimSection = currentSection.startsWith('[fltsim.');
-						if (isFltsimSection) {
-							currentFltsimEntry = createEmptyEntry();
-							currentFltsimEntry['_header'] = line;
-						}
-					}
+	const removeProperties = new Map(
+		config.cleanAircraftCfg.removeUnusedLinesItems.map((prop: string) => prop.split('='))
+	);
 
-					// Handle line
-					if (currentSection && isFltsimSection) {
-						if (!isSectionStart) {
-							let add = true;
+	// Split lines and iterate through
+	for (let [i, line] of splitText.entries()) {
+		line = line.trim();
 
-							const prop = line.split('=');
-							let key = prop[0].toLowerCase();
-							let value = prop[1];
-
-							// Remove unused lines
-							if (config.cleanAircraftCfg.removeUnusedLines && removeProperties.has(key)) {
-								let removeValue = removeProperties.get(key);
-								if (removeValue === undefined) {
-									add = false;
-								} else if (removeValue === '*' && value) {
-									add = false;
-								} else if (removeValue === '_' && !value) {
-									add = false;
-								} else if (removeValue === value) {
-									add = false;
-								}
-							}
-
-							// Callsign uppercase
-							if (config.cleanAircraftCfg.callsignsUppercase && key === 'atc_airline') {
-								value = value.toUpperCase();
-							}
-
-							if (key === 'model') {
-								key = 'model';
-							}
-
-							// Add to fltsim entry
-							if (add) {
-								currentFltsimEntry[key] = value;
-							}
-						}
-					} else {
-						cleanTextArray.push(line);
-					}
-				} else if (currentSection && isFltsimSection) {
-					//ignore line
-				} else {
-					cleanTextArray.push(line);
-				}
-
-				// Last line, push entry to list if necessary
-				if (i === splitText.length - 1 && currentSection && isFltsimSection) {
+		if (line.length > 0) {
+			let isSectionStart = line.startsWith('[') && line.endsWith(']');
+			// New section start
+			if (isSectionStart) {
+				// Close old section and push to list
+				if (currentSection && isFltsimSection) {
 					fltsimEntries.push(currentFltsimEntry);
 				}
-			});
 
-			// Renumber
-			if (config.cleanAircraftCfg.renumber) {
-				fltsimEntries.forEach((entry, i) => {
-					entry['_header'] = `[fltsim.${i}]`;
-				});
+				currentSection = line;
+				isFltsimSection = currentSection.startsWith('[fltsim.');
+				if (isFltsimSection) {
+					currentFltsimEntry = createEmptyEntry();
+					currentFltsimEntry['_header'] = line;
+				}
 			}
 
-			// Add to text array in sorted order
-			if (config.cleanAircraftCfg.sortProperties) {
-				const sortProperties: string[] = config.cleanAircraftCfg.sortPropertiesOrder.map((item: string) =>
-					item.trim()
-				);
+			// Handle line
+			if (currentSection && isFltsimSection) {
+				if (!isSectionStart) {
+					let add = true;
 
-				fltsimEntries.forEach((entry, i) => {
-					const props = [entry._header];
-					delete entry._header;
+					const prop = line.split('=');
+					let key = prop[0].toLowerCase();
+					let value = prop[1];
 
-					// First the sort keys...
-					for (const key of sortProperties) {
-						if (key in entry) {
-							props.push(`${key}=${entry[key]}`);
-							delete entry[key];
+					// Remove unused lines
+					if (config.cleanAircraftCfg.removeUnusedLines && removeProperties.has(key)) {
+						let removeValue = removeProperties.get(key);
+						if (removeValue === undefined) {
+							add = false;
+						} else if (removeValue === '*' && value) {
+							add = false;
+						} else if (removeValue === '_' && !value) {
+							add = false;
+						} else if (removeValue === value) {
+							add = false;
 						}
 					}
 
-					// ... then the remaining ones
-					for (const prop in entry) {
-						props.push(`${prop}=${entry[prop]}`);
+					if (add) {
+						// Callsign uppercase
+						if (config.cleanAircraftCfg.callsignsUppercase && key === 'atc_airline') {
+							value = value.toUpperCase();
+						}
+
+						// Add to fltsim entry
+						currentFltsimEntry[key] = value;
 					}
-
-					cleanTextArray.push(...props, '');
-				});
+				}
 			} else {
+				cleanTextArray.push(line);
 			}
+		} else if (currentSection && isFltsimSection) {
+			//ignore line
+		} else {
+			cleanTextArray.push(line);
+		}
 
-			const cleanText = cleanTextArray.join('\n');
-
-			// Apply changes to document
-			editor.edit((editBuilder) => {
-				editBuilder.replace(new vscode.Range(0, 0, document.lineCount, 500), cleanText);
-			});
-			vscode.window.showInformationMessage('Aicraft.cfg cleaned');
+		// Last line, push entry to list if necessary
+		if (i === splitText.length - 1 && currentSection && isFltsimSection) {
+			fltsimEntries.push(currentFltsimEntry);
 		}
 	}
+
+	// Renumber
+	if (config.cleanAircraftCfg.renumber) {
+		fltsimEntries.forEach((entry, i) => {
+			entry['_header'] = `[fltsim.${i}]`;
+		});
+	}
+
+	// Add to text array in sorted order
+	if (config.cleanAircraftCfg.sortProperties) {
+		for (const entry of fltsimEntries) {
+			const props = [entry._header];
+			delete entry._header;
+
+			// First the sort keys...
+			props.push(
+				...config.cleanAircraftCfg.sortPropertiesOrder
+					.filter((key: string) => key in entry)
+					.map((key: string) => {
+						const ret = `${key}=${entry[key]}`;
+						delete entry[key];
+						return ret;
+					})
+			);
+
+			// ... then the remaining ones
+			props.push(...Object.keys(entry).map((key) => `${key}=${entry[key]}`));
+
+			cleanTextArray.push(...props, '');
+		}
+	} else {
+		// Add to text array in original order
+		for (const entry of fltsimEntries) {
+			cleanTextArray.push(
+				...Object.entries(entry).map(([key, value]) => (key === '_header' ? value : `${key}=${value}`)),
+				''
+			);
+		}
+	}
+
+	// Apply changes to document
+	editor.edit((editBuilder) => {
+		editBuilder.replace(new vscode.Range(0, 0, document.lineCount, 500), cleanTextArray.join('\n'));
+	});
+	vscode.window.showInformationMessage('Aicraft.cfg cleaned');
 }
 
 function createEmptyEntry() {
-	let entry: FltsimEntry = {};
-	return entry;
+	return <FltsimEntry>{};
 }
