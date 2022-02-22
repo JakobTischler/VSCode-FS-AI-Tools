f71ee47a87b93bde4fe054a8d9f0a78f23e689d1
Jakob Tischler <jakob.tischler@gmail.com>
2022-02-22 13:08:53 +0100

    Dependency updates

--- src/ext/number.ts
+++
@@ -1,7 +0,0 @@
-interface Number {
-	pad(maxLength: number, fillString?: string): string;
-}
-
-Number.prototype.pad = function (maxLength: number, fillString: string = '0'): string {
-	return `${this}`.padStart(maxLength, fillString);
-};
